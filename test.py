57b06e25ea580e88c4af259238690311077eb111
Andrew Kolarits <amkolarits0222@gmail.com>
2025-12-05 18:43:58 -0500

    delete files

--- test.py
+++
@@ -1,8 +0,0 @@
-import torch
-import torch_directml
-
-print("Torch version:", torch.__version__)
-
-dml = torch_directml.device()  # DirectML device
-x = torch.randn(2, 3).to(dml)
-print("Tensor device:", x.device)
